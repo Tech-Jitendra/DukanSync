40cd5956ee2b8715819435e849820be82b48edfd
jitendra-maurya <jitenmaurya0@gmail.com>
2025-06-29 01:21:56 +0530

    feat: update tab layout by adding Details and User screens; modify Settings tab text

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,7 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 import Feather from '@expo/vector-icons/Feather';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
@@ -17,6 +19,20 @@ export default function TabLayout() {
                 options={{
                     title: 'Settings',
                     tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="(home)/details"
+                options={{
+                    title: 'Details',
+                    tabBarIcon: ({ color }) => <AntDesign name="infocirlceo" size={24} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="user/index"
+                options={{
+                    title: 'User',
+                    tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account" size={24} color={color} />,
                 }}
             />
         </Tabs>
